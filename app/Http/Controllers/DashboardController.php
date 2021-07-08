cbe782fb7e169f091815e62678faca8fb6ed491a
Dany Adhi Prabowo <danyadhi4149@gmail.com>
2021-07-08 16:05:04 +0700

    add feature CRUD faculty

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class DashboardController extends Controller
 {
     public function index(){
         return view('dashboard.index');
-        
     }
 }
